d5b45204b8e86819e391c24062096bce52bddbbb
neilalberg <neilalberg@gmail.com>
2016-04-19 10:39:46 -0400

    Added onClicks for the Statistics buttons
    (so that they don't crash during the in-class demo)

--- DonationAppAndroid/app/src/main/java/com/example/psweeney/donationappandroid/StatisticsActivity.java
+++ DonationAppAndroid/app/src/main/java/com/example/psweeney/donationappandroid/StatisticsActivity.java
@@ -1,7 +1,9 @@
 package com.example.psweeney.donationappandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+    }
+
+    public void onClickCategoryGoals(View v){
+        startActivity(new Intent(getApplicationContext(), StatisticsActivity.class));
+    }
+
+    public void onClickCategoryHistory(View v){
+        startActivity(new Intent(getApplicationContext(), StatisticsActivity.class));
+    }
+
+    public void onClickCategoryBreakdown(View v){
+        startActivity(new Intent(getApplicationContext(), StatisticsActivity.class));
     }
 }
