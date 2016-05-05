91e24fd3e1a6f79b9fd02991301851fa5050410c
neilalberg <neilalberg@gmail.com>
2016-05-05 19:42:31 -0400

    Layout of charity search
    Added the layout for how the charities will be displayed in the
    recommended charities

--- DonationAppAndroid/app/src/main/java/com/example/psweeney/donationappandroid/SearchActivity.java
+++ DonationAppAndroid/app/src/main/java/com/example/psweeney/donationappandroid/SearchActivity.java
@@ -4,15 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.example.psweeney.donationappandroid.charity.CharityDetailData;
+import com.example.psweeney.donationappandroid.charity.CharityProfileAdapter;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private CharityDetailData _data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-
-
-
     }
 }
